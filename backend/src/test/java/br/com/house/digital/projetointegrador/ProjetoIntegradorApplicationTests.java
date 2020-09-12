103a5592c531e1b083b84f39f9088d2e241c5881
ggwadera <16023489+ggwadera@users.noreply.github.com>
2020-09-12 11:18:05 -0300

    projeto spring boot base

---
+++ backend/src/test/java/br/com/house/digital/projetointegrador/ProjetoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.house.digital.projetointegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
