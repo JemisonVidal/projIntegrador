3dfabd9c2f9de9286237087d320e4073cc04cdfd
Jemison Vidal <jemison.santoos@gmail.com>
2020-09-13 08:28:50 -0300

    PI-5 - Tela de Registro
    Implementado validações e adicionado endpoint para cadastro.

--- frontend/src/components/Input/Input.jsx
+++ frontend/src/components/Input/Input.jsx
@@ -2,14 +2,35 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 import './Input.css';
 
-const Input = (props) => {
+const Input = ({ namelabel, type, as, value, onChange, onBlur, options, error, required }) => {
   return (
     <>
-      <Form.Label className="label">{props.nameLabel}</Form.Label>
-      <Form.Control className="Input"{...props}>
-        {props.options && props.options.map(op => <option>{op}</option>)}
-
-      </Form.Control>
+      <Form.Label className="label" htmlFor={namelabel}>{namelabel}</Form.Label>
+      {type
+        ? <Form.Control
+          className="Input"
+          id={namelabel}
+          name={namelabel}
+          type={type}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          required={required}
+        />
+        : <Form.Control
+          className="Input"
+          id={namelabel}
+          name={namelabel}
+          as={as}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          required={required}
+        >
+          {options && options.map(op => <option key={op}>{op}</option>)}
+        </Form.Control>
+      }
+      {error && <p className="error">{error}</p>}
     </>
   );
 }
--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -1,31 +1,74 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Spinner } from 'react-bootstrap';
+import { useHistory, Link } from 'react-router-dom';
 import Input from '../../components/Input/Input';
 import Checkbox from '../../components/Checkbox/Checkbox';
+import Error from '../../components/Helper/Error';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { USER_CREATE } from '../../APIs/APIs';
 import './Register.css';
 
 const UserRegister = () => {
-  const options = [
+  const nome = useForm();
+  const email = useForm("email");
+  const senha = useForm();
+  const confirmarSenha = useForm();
+  const tipo = useForm();
+  const termo = useForm();
+  const { loading, error, request } = useFetch();
+  const history = useHistory();
+
+  const optionsComboBox = [
     "Escolha uma opção",
     "Quero trabalhar",
     "Quero recrutar"
   ];
 
+  async function registrar(nome, email, senha, tipo) {
+    const { url, options } = USER_CREATE({ nome, email, senha, tipo });
+    await request(url, options);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (nome.validate() && email.validate() &&
+      senha.validate() && confirmarSenha.validate() &&
+      tipo.validate()) {
+      const { response } = await registrar(nome, email, senha, tipo);
+      if (response.ok) return history.push('/login');
+    }
+  }
+
   return (
     <div className="container-box">
-      <Form className="container form-box">
+      <Form className="container form-box" onSubmit={handleSubmit}>
         <h1 className="titulo-registro">Cadastre-se</h1>
-        <Input nameLabel="Nome" type="text" placeholder="Nome" required />
-        <Input nameLabel="Email" type="email" placeholder="Email" required />
-        <Input nameLabel="Senha" type="password" placeholder="Senha" required />
-        <Input nameLabel="Confirmar Senha" type="password" placeholder="Confirmar Senha" required />
-        <Input nameLabel="Quero trabalhar/Quero recrutar" as="select" required options={options} />
-        <Checkbox className="CheckBoxMin" type="checkbox" label="Eu concordo com os termos de serviço e políticas de privacidade."></Checkbox>
-        <Button id="Cadastrar" variant="primary" type="submit">
-          Cadastrar
-        </Button>
+        <Input namelabel="Nome" type="text" placeholder="Nome" required  {...nome} />
+        <Input namelabel="Email" type="email" placeholder="Email" required {...email} />
+        <Input namelabel="Senha" type="password" placeholder="Senha" required {...senha} />
+        <Input namelabel="Confirmar Senha" type="password" placeholder="Confirmar Senha" required {...confirmarSenha} />
+        <Input namelabel="Quero trabalhar/Quero recrutar" as="select" required options={optionsComboBox}  {...tipo} />
+        <Checkbox className="CheckBoxMin" type="checkbox" required label="Eu concordo com os termos de serviço e políticas de privacidade." />
+        {loading
+          ? (<Button id="Cadastrar" variant="primary" disabled>
+            <Spinner
+              as="span"
+              animation="border"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+            <span className="sr-only">Carregando...</span>
+          </Button>)
+          : (<Button id="Cadastrar" variant="primary" type="submit">
+            Cadastrar
+          </Button>)
+        }
+        <Error error={error} />
+        <p id="cadastro">Já tem cadastro ? <Link id="login" to="/login">Faça o Login</Link></p>
       </Form>
-
     </div >
   )
 }
