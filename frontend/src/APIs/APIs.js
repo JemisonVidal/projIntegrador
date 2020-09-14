516219d6964ec853f331fd9dd683be4aed3320b4
AntonioGSC <antoniogabrielagsc@gmail.com>
2020-09-13 21:43:01 -0300

    PI-6 - Tela Login
    Construção da Tela de Login

--- frontend/src/APIs/APIs.js
+++ frontend/src/APIs/APIs.js
@@ -1,10 +1,23 @@
-export const baseURL = "http://localhost:8080/";
+export const baseURL = 'http://localhost:8080/';
 
 export function USER_CREATE(body) {
   return {
     url: baseURL + '/v1/api/register',
     options: {
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export function USER_LOGIN(body) {
+  return {
+    url: baseURL + '/v1/api/login',
+    options: {
+      method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
