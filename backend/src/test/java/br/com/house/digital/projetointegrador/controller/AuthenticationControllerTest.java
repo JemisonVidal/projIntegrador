341d06c7ec772a60e0a0f959288fed87031bf662
Luís Carlos Sílva Júnior <luisccomp@gmail.com>
2020-10-05 18:45:37 -0300

    Create authentication controller test and updates authentication controller structure

---
+++ backend/src/test/java/br/com/house/digital/projetointegrador/controller/AuthenticationControllerTest.java
@@ -0,0 +1,15 @@
+package br.com.house.digital.projetointegrador.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles({"test"})                                 // Tell that our current configuration file is "application-test.properties"
+@WebMvcTest(controllers = AuthenticationController.class) // This annotation configures our class to test a controller
+@AutoConfigureMockMvc                                     // This annotation will configure an object where we can simulate requests
+public class AuthenticationControllerTest {
+    
+}
