cc8c258d381a9620192c3af41591730fac2a64e7
AntonioGSC <antoniogabrielagsc@gmail.com>
2020-10-04 22:16:56 -0300

    PI - 20 - Carousel Home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 import Main from '../../components/Template/main/Main';
 import imageMain from '../../assets/images/main-photo.jpg'
 import logo from '../../assets/images/Logo2RecruIT.png'
@@ -14,12 +15,31 @@ const PagesHome = () => {
         <h1>Recru<span>IT</span></h1>
         <p>Descubra e seja descoberto</p>
       </div>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum quam eligendi officiis? Quo enim eius magnam numquam modi. Voluptas, et in fuga voluptate repellat accusamus voluptatem voluptatum hic excepturi tempore.</p>
-      <p>Autem quod saepe iste excepturi eaque eveniet, qui aperiam aliquid? Mollitia exercitationem ducimus fuga! Ratione alias voluptate dolor, dolores a eligendi libero ea quas nesciunt sequi ullam natus illo fuga.</p>
-      <p>Vel adipisci vitae, unde dicta iure illo atque, fuga mollitia nihil excepturi voluptatum eveniet rerum quae laborum magnam minus. Quaerat, rerum quasi officia tempora numquam incidunt iste quis inventore harum?</p>
-      <p>Voluptates vitae beatae sed, veritatis tempora voluptate! Sint minima molestias ducimus consequatur sapiente possimus quod harum tempora consectetur aut magnam, totam fuga alias dolores aperiam quibusdam eveniet ea quisquam corrupti.</p>
-      <p>Ipsum aut iusto expedita qui dicta veritatis, autem .</p>
-    </Main>
+      <div>
+        <Carousel className="carousel" indicators={true}>
+          <Carousel.Item>
+            <div className="divCarousel">
+              <a href="" className="titleCarousel">Adicionar Habilidades</a>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="divCarousel">
+              <a href="" className="titleCarousel">Adicionar Cursos</a>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="divCarousel">
+              <a href="" className="titleCarousel">Adicionar Experiências</a>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="divCarousel">
+              <a href="" className="titleCarousel">Adicionar Links Externos</a>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </Main >
   );
 };
 
