94ab4f89c90ac9f72adf91a57ab6d54698d23519
Luís Carlos Sílva Júnior <luisccomp@gmail.com>
2020-10-08 20:59:58 -0300

    Working on tests

--- backend/src/test/java/br/com/house/digital/projetointegrador/controller/AuthenticationControllerTest.java
+++ backend/src/test/java/br/com/house/digital/projetointegrador/controller/AuthenticationControllerTest.java
@@ -3,13 +3,11 @@ package br.com.house.digital.projetointegrador.controller;
 import br.com.house.digital.projetointegrador.model.TypeEnum;
 import br.com.house.digital.projetointegrador.model.User;
 import br.com.house.digital.projetointegrador.model.UserDTO;
-import br.com.house.digital.projetointegrador.repository.UserRepository;
 import br.com.house.digital.projetointegrador.security.JWTAuthenticationEntryPoint;
 import br.com.house.digital.projetointegrador.security.JWTRequestFilter;
 import br.com.house.digital.projetointegrador.security.JWTTokenUtil;
 import br.com.house.digital.projetointegrador.security.WebSecurityConfig;
 import br.com.house.digital.projetointegrador.service.AuthenticationService;
-import br.com.house.digital.projetointegrador.service.impl.UserDetailsServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -27,60 +27,68 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@ActiveProfiles({"test"})                                 // Tell that our current configuration file is "application-test.properties"
-@WebMvcTest(controllers = AuthenticationController.class) // This annotation configures our class to test a controller
-@AutoConfigureMockMvc                                     // This annotation will configure an object where we can simulate requests
-@ContextConfiguration(classes = {JWTAuthenticationEntryPoint.class,
-        JWTRequestFilter.class,
-        JWTTokenUtil.class,
+@ActiveProfiles({"test"})
+@WebMvcTest(controllers = AuthenticationController.class)
+@AutoConfigureMockMvc
+@Import(AuthenticationController.class)
+@ContextConfiguration(classes = {
+        JWTAuthenticationEntryPoint.class,
         WebSecurityConfig.class,
-        UserDetailsServiceImpl.class,
-        UserRepository.class
+        JWTRequestFilter.class,
+        JWTTokenUtil.class
 })
 public class AuthenticationControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    MockMvc mvc;
 
     @MockBean
     AuthenticationService authenticationService;
 
+    @MockBean
+    UserDetailsService userDetailsService;
+
     ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    @DisplayName("Should register a user on database and return status 201")
-    public void testRegisterUser() throws Exception {
-        UserDTO userDTO = UserDTO.builder()
-                .email("natasha.romanov@gmail.com")
+    @DisplayName("Should register a user on database and return status 201.")
+    public void registerUserTest() throws Exception {
+        UserDTO dto = UserDTO.builder()
                 .name("Natasha Romanov")
-                .password("BlackWidow2020")
+                .email("black_widow@shield.com.br")
+                .password("WidowShield2020")
                 .type(TypeEnum.USER)
                 .build();
 
-        String json = objectMapper.writeValueAsString(userDTO);
-
-        when(authenticationService.save(any())).thenReturn(User.builder()
+        User user = User.builder()
                 .uuid(UUID.randomUUID())
-                .email(userDTO.getEmail())
-                .name(userDTO.getName())
-                .password(userDTO.getPassword())
-                .build());
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .type(dto.getType())
+                .build();
+
+        when(authenticationService.save(any(User.class))).thenReturn(user);
 
         RequestBuilder request = post("/v1/api/register")
                 .header("Content-type", "application/json")
-                .header("Accept", "application/json")
-                .content(json);
+                .content(objectMapper.writeValueAsString(dto));
 
-        mockMvc.perform(request)
+        mvc.perform(request)
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("name").value(userDTO.getName()))
-                .andExpect(jsonPath("email").value(userDTO.getEmail()));
+                .andExpect(jsonPath("name").value(dto.getName()))
+                .andExpect(jsonPath("email").value(dto.getEmail()))
+                .andExpect(jsonPath("type").value(TypeEnum.USER.toString()));
+
+        verify(authenticationService, times(1)).save(any(User.class));
     }
 
 }
