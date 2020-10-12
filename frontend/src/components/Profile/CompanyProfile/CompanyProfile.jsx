89247e7c8e8ce38dd96129c2893bfebdd3740e79
Jemison Vidal <jemison.santoos@gmail.com>
2020-10-12 09:14:59 -0300

    ✨feature: Implementado o perfil da empresa

--- frontend/src/components/Profile/CompanyProfile/CompanyProfile.jsx
+++ frontend/src/components/Profile/CompanyProfile/CompanyProfile.jsx
@@ -1,17 +1,52 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 import ProfileCard from '../ProfileCard/ProfileCard';
+import './CompanyProfile.css'
 
 const CompanyProfile = ({ data }) => {
 
-  const generalInformation = {
-    'Local': data.city,
+  const about = {
+    'Sobre': data.about,
   }
 
+  const additional = {
+    "Endereço": data.address,
+    "Tempo de existência": data.lifetime,
+    "Ramo": data.branch,
+    "Site": data.site
+  }
   return (
     <div>
-      <ProfileCard title='Informações Gerais' data={generalInformation} />
+      <ProfileCard title='Visão Geral' data={about} />
+      <ProfileCard title='Informações Gerais' data={additional} />
+      {
+        data.opportunitys &&
+        <div className="opportunitys">
+          <div className="sidebar" >
+            {renderOpportunitys(data.opportunitys)}
+          </div>
+        </div>
+      }
     </div>
   )
 }
+
+const renderOpportunitys = (opportunitys) => {
+  return (
+    opportunitys.map(o => {
+      const data = {
+        description: o.description,
+        activities: o.activities
+      }
+      return (
+        <div key={o.id}>
+          <ProfileCard title={o.title} data={data} />
+        </div>
+      )
+    })
+  )
+}
+
+
 
 export default CompanyProfile;
--- frontend/src/components/Profile/ProfileCard/ProfileCardItem/ProfileCardItem.jsx
+++ frontend/src/components/Profile/ProfileCard/ProfileCardItem/ProfileCardItem.jsx
@@ -8,15 +8,15 @@ const ProfileCardItem = ({ title, value }) => {
         <span className="font-weight-bold">{title}</span>
       </Col>
       <Col sm={12} md>
-        {typeof value === 'string' ? (
+        {typeof value === 'string' || typeof value === 'number' ? (
           <span>{value}</span>
         ) : (
-          value.map((v, i) => (
-            <span key={i} className="d-block">
-              {v}
-            </span>
-          ))
-        )}
+            value.map((v, i) => (
+              <span key={i} className="d-block">
+                {v}
+              </span>
+            ))
+          )}
       </Col>
     </Row>
   );
