ccb187042344b9d8218076e6a43acb760fdeedcb
jemisonsantos <jemison.santos@catho.com.br>
2020-10-17 11:13:38 -0300

    Implementado avatar no navbar

--- frontend/src/components/Template/nav/Nav.jsx
+++ frontend/src/components/Template/nav/Nav.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import { Navbar, Nav, Image } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import './Nav.css'
 
-const NavBar = (props) => {
+const NavBar = ({ avatar, type, id }) => {
+  //TODO: Confirmar se iremos receber props ou utilizar o context/redux
+  const mockPropAtavar = `http://placekitten.com/300/300`;
+  type = `user`
+  id = 1
+
   return (
     <>
       <Navbar className="Navbar" collapseOnSelect expand="lg"  >
-        <Navbar.Brand className="nav-item" href="#home">RecruIT</Navbar.Brand>
+        <Link to={`/`}>
+          <Navbar.Brand className="nav-item" href="#home">RecruIT</Navbar.Brand>
+        </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link className="nav-item" href="#Oportunidades">Oportunidades</Nav.Link>
             <Nav.Link className="nav-item" href="#Empresas">Empresas</Nav.Link>
+          </Nav>
+          <Nav className="ml-auto">
+            <Link to={`/profile/${type}/${id}`}>
+              <Image className="avatar" src={mockPropAtavar} rounded />
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
