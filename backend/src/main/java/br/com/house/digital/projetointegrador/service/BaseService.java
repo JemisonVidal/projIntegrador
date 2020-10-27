9caeb9566b78734eaf5a9d2e390a8de33080af97
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-10-26 21:54:41 -0300

    #50 Repositório, serviço e controlador p/ perfil empresa

---
+++ backend/src/main/java/br/com/house/digital/projetointegrador/service/BaseService.java
@@ -0,0 +1,26 @@
+package br.com.house.digital.projetointegrador.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface BaseService<T, PK> {
+
+    T save(T entity);
+
+    T update(T entity);
+
+    T findById(PK id);
+
+    List<T> findAll();
+
+    Page<T> findAll(Pageable pageable);
+
+    void deleteById(PK id);
+
+    T convertToEntity(Object dto);
+
+    <D> D convertFromEntity(T entity, Class<D> destinationType);
+
+}
---
+++ backend/src/main/java/br/com/house/digital/projetointegrador/service/impl/BaseServiceImpl.java
@@ -0,0 +1,71 @@
+package br.com.house.digital.projetointegrador.service.impl;
+
+import br.com.house.digital.projetointegrador.model.AbstractEntity;
+import br.com.house.digital.projetointegrador.service.BaseService;
+import br.com.house.digital.projetointegrador.service.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.transaction.Transactional;
+import java.io.Serializable;
+import java.util.List;
+
+public abstract class BaseServiceImpl<T extends AbstractEntity<PK>, PK extends Serializable> implements BaseService<T, PK> {
+
+    protected final JpaRepository<T, PK> repository;
+    private final ModelMapper modelMapper;
+    private final Class<T> genericType;
+
+    public BaseServiceImpl(JpaRepository<T, PK> repository, ModelMapper modelMapper, Class<T> genericType) {
+        this.repository = repository;
+        this.modelMapper = modelMapper;
+        this.genericType = genericType;
+    }
+
+    @Override
+    @Transactional
+    public T save(T entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public T update(T entity) {
+        findById(entity.getId());
+        return save(entity);
+    }
+
+    @Override
+    public T findById(PK id) {
+        return repository.findById(id)
+            .orElseThrow(() -> new ObjectNotFoundException("Object not found with id: " + id));
+    }
+
+    @Override
+    public List<T> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Page<T> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Override
+    public void deleteById(PK id) {
+        final T entity = findById(id);
+        repository.delete(entity);
+    }
+
+    @Override
+    public T convertToEntity(Object dto) {
+        return modelMapper.map(dto, this.genericType);
+    }
+
+    @Override
+    public <D> D convertFromEntity(T entity, Class<D> destinationType) {
+        return modelMapper.map(entity, destinationType);
+    }
+
+}
