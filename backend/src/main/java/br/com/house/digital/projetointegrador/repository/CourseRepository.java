4e5facc72b23861513409b6b6b29e25fbd8ba41b
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-10-26 21:55:52 -0300

    #50 Refatoração repositórios

--- backend/src/main/java/br/com/house/digital/projetointegrador/repository/CourseRepository.java
+++ backend/src/main/java/br/com/house/digital/projetointegrador/repository/CourseRepository.java
@@ -3,7 +3,7 @@ package br.com.house.digital.projetointegrador.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.house.digital.projetointegrador.model.Course;
+import br.com.house.digital.projetointegrador.model.profile.Course;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
