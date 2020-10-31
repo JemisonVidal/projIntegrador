d5feb9fa5777e611f50e878137b451c1ebec1e97
jemisonsantos <jemison.santos@catho.com.br>
2020-10-31 09:45:27 -0300

    🐞fix: ajuste base layout

--- frontend/src/components/Template/main/Main.jsx
+++ frontend/src/components/Template/main/Main.jsx
@@ -1,17 +1,19 @@
-import React, { Children } from 'react';
-import './Main.css';
-import Footer from '../footer/Footer';
-import logo from '../../../assets/images/LogoRecruIT.png'
+import React, { Children } from "react";
+import "./Main.css";
+import Footer from "../footer/Footer";
+import logo from "../../../assets/images/LogoRecruIT.png";
+import Nav from "../nav/Nav";
 
-export default props =>
-    <React.Fragment>
-        <main className="content container-box-home">
-            <div className="container-image-background">
-                <div className="container form-box-home">
-                    {props.children}
-                    <Footer />
-                </div>
-            </div>
-        </main>
-
-    </React.Fragment>
+export default (props) => (
+  <React.Fragment>
+    <main className="content container-box-home">
+      <div className="container-image-background">
+        <div className="container form-box-home">
+          <Nav />
+          {props.children}
+          <Footer />
+        </div>
+      </div>
+    </main>
+  </React.Fragment>
+);
