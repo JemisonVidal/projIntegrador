ef1c295b9a477fdc53185341eb82ab837bdfe134
jemisonsantos <jemison.santos@catho.com.br>
2020-11-08 11:20:44 -0300

    ✨feature: Add home for companyy

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card, CardDeck, Button } from "react-bootstrap";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import Main from "../../components/Template/main/Main";
 import imageMain from "../../assets/images/main-photo.jpg";
 import logo from "../../assets/images/Logo2RecruIT.png";
@@ -8,21 +8,21 @@ import skillSvg from "../../assets/images/home/skills.svg";
 import courseSvg from "../../assets/images/home/course.svg";
 import linkSvg from "../../assets/images/home/link.svg";
 import workSvg from "../../assets/images/home/work.svg";
+import searchSvg from "../../assets/images/home/search.svg";
+import opportunitySvg from "../../assets/images/home/opportunity.svg";
+import aboutSvg from "../../assets/images/home/about.svg";
 
 import "./Home.css";
 
 const PagesHome = () => {
-  var image = "http://placekitten.com/300/300";
-  return (
-    <Main>
-      <div className="div-banner-home">
-        <img className="image-banner-home" src={imageMain} />
-        <img className="logo-banner-home" src={logo} />
-        <h1>
-          Recru<span>IT</span>
-        </h1>
-        <p>Descubra e seja descoberto</p>
-      </div>
+  const history = useHistory();
+
+  const renderProfileUser = () => {
+    function handleOnClick(event) {
+      return history.push(`/profile/user/${1}`);
+    }
+
+    return (
       <section className="data-profile">
         <CardDeck>
           <Card>
@@ -33,7 +33,9 @@ const PagesHome = () => {
                 Acessar minhas habilidades(Pensar em uma frase)
               </Card.Text>
             </Card.Body>
-            <Button variant="primary">Acessar minhas habilidades</Button>
+            <Button variant="primary" onClick={handleOnClick}>
+              Acessar minhas habilidades
+            </Button>
           </Card>
           <Card>
             <Card.Title>Cursos</Card.Title>
@@ -41,7 +43,9 @@ const PagesHome = () => {
             <Card.Body>
               <Card.Text>Acessar meus cursos(Pensar em uma frase)</Card.Text>
             </Card.Body>
-            <Button variant="primary">Acessar meus cursos</Button>
+            <Button variant="primary" onClick={handleOnClick}>
+              Acessar meus cursos
+            </Button>
           </Card>
         </CardDeck>
         <CardDeck>
@@ -53,7 +57,9 @@ const PagesHome = () => {
                 Acessar meus links externos (Pensar em uma frase)
               </Card.Text>
             </Card.Body>
-            <Button variant="primary">Acessar meus links externos</Button>
+            <Button variant="primary" onClick={handleOnClick}>
+              Acessar meus links externos
+            </Button>
           </Card>
           <Card>
             <Card.Title>Experiências</Card.Title>
@@ -63,10 +69,73 @@ const PagesHome = () => {
                 Acessar minhas Experiências(Pensar em uma frase)
               </Card.Text>
             </Card.Body>
-            <Button variant="primary">Acessar minhas Experiências</Button>
+            <Button variant="primary" onClick={handleOnClick}>
+              Acessar minhas Experiências
+            </Button>
           </Card>
         </CardDeck>
       </section>
+    );
+  };
+
+  const renderProfileCompany = () => {
+    function handleOnClick(event) {
+      return history.push(`/profile/company/${1}`);
+    }
+
+    return (
+      <section className="data-profile">
+        <CardDeck>
+          <Card>
+            <Card.Title>Adicionar Vaga</Card.Title>
+            <Card.Img variant="top" src={opportunitySvg} />
+            <Card.Body>
+              <Card.Text>Adicionar vaga(Pensar em uma frase)</Card.Text>
+            </Card.Body>
+            <Button variant="primary" onClick={handleOnClick}>
+              Adicionar Vaga
+            </Button>
+          </Card>
+          <Card>
+            <Card.Title>Procurar candidata</Card.Title>
+            <Card.Img variant="top" src={searchSvg} />
+            <Card.Body>
+              <Card.Text>Procurar candidata(Pensar em uma frase)</Card.Text>
+            </Card.Body>
+            <Button variant="primary" onClick={handleOnClick}>
+              Procurar Candidata
+            </Button>
+          </Card>
+        </CardDeck>
+        <CardDeck>
+          <Card>
+            <Card.Title>Sobre</Card.Title>
+            <Card.Img variant="top" src={aboutSvg} />
+            <Card.Body>
+              <Card.Text>Acessar sobre(Pensar em uma frase)</Card.Text>
+            </Card.Body>
+            <Button variant="primary" onClick={handleOnClick}>
+              Sobre
+            </Button>
+          </Card>
+        </CardDeck>
+      </section>
+    );
+  };
+
+  var image = "http://placekitten.com/300/300";
+  return (
+    <Main>
+      <div className="div-banner-home">
+        <img className="image-banner-home" src={imageMain} />
+        <img className="logo-banner-home" src={logo} />
+        <h1>
+          Recru<span>IT</span>
+        </h1>
+        <p>Descubra e seja descoberto</p>
+      </div>
+      {renderProfileCompany()}
+      {/* TODO: ADICIONAR O HOME DA EMPRESA COM OS CARDS CRIAR VAGA/BUSCAR CANDIDATA */}
     </Main>
   );
 };
