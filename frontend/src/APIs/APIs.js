3b9c60df43ea5d72322bb9f1ec278156f44ff464
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-11-11 20:14:08 -0300

    front: rotas API perfil

--- frontend/src/APIs/APIs.js
+++ frontend/src/APIs/APIs.js
@@ -2,25 +2,30 @@
 // TODO arrumar o CORS no back, por enquanto ta usando o proxy no ambiente dev
 export const baseURL = '';
 
-const requestOptions = ({url, method = 'GET', body, jwt}) => {
-  return {
+const requestOptions = ({url, method = 'GET', body, isAuthenticated = true}) => {
+  const jwt = localStorage.getItem('apptoken');
+  const request = {
     url: baseURL + url,
     options: {
       method: method,
       headers: {
         'Content-Type': 'application/json',
-        Authorization: jwt ? 'Bearer ' + jwt : undefined,
       },
       body: body ? JSON.stringify(body) : undefined
     }
   }
+  if (isAuthenticated) {
+    request.options.headers['Authorization'] = 'Bearer ' + JSON.parse(jwt);
+  }
+  return request;
 }
 
 export function USER_REGISTER(body) {
   return requestOptions({
     url: '/v1/api/register',
     method: 'POST',
-    body
+    body,
+    isAuthenticated: false
   })
 }
 
@@ -28,14 +33,28 @@ export function USER_LOGIN(body) {
   return requestOptions({
     url: '/v1/api/authenticate',
     method: 'POST',
-    body
+    body,
+    isAuthenticated: false
   })
 }
 
-export function GET_PROFILE(type, id, jwt) {
-  const url = `/v1/api/profile/${type}/${id}`
+export function GET_AVATAR(type, id) {
   return requestOptions({
-    url,
-    jwt
+    url: `/v1/api/profile/${type}/${id}/avatar`,
+    isAuthenticated: false
+  })
+}
+
+export function GET_PROFILE(type, id) {
+  return requestOptions({
+    url: `/v1/api/profile/${type}/${id}`
+  })
+}
+
+export function PATCH_PROFILE(type, id, body) {
+  return requestOptions({
+    url: `/v1/api/profile/${type}/${id}`,
+    method: 'PATCH',
+    body
   })
 }
