8443c4296981ea5f0af6767c7bb97be48c104846
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2020-11-12 17:37:36 -0300

    front: refactor perfil

--- frontend/src/components/Profile/ApplicantProfile/ApplicantInfo.jsx
+++ frontend/src/components/Profile/ApplicantProfile/ApplicantInfo.jsx
@@ -1,53 +1,51 @@
 import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
 import ProfileCard from '../ProfileCard/ProfileCard';
 import ProfileCardItem from '../ProfileCard/ProfileCardItem';
+import useEditMode from '../../../Hooks/useEditMode';
 
 const ApplicantInfo = ({ data, setData, canEdit, handleSubmit }) => {
-  const [isEditing, setEditing] = useState(false);
+  const { editing, setEditing, toggleEditMode } = useEditMode();
   const [newData, setNewData] = useState({});
   const title = 'Informações Pessoais';
 
-  function handleClick(event) {
-    event.preventDefault();
-    if (!isEditing) setEditing(true);
-    else if (window.confirm('Cancelar alterações?')) setEditing(false);
-  }
-  
   const handleSave = async () => {
     const { response } = await handleSubmit(newData);
     if (response.ok) updateData();
-  }
+  };
 
   const updateData = () => {
-    const copy = {...data};
-    Object.entries(newData).forEach(([k, v]) => copy[k].value = v)
+    const copy = { ...data };
+    Object.entries(newData).forEach(([k, v]) => (copy[k].value = v));
     setData(copy);
     setNewData({});
     setEditing(false);
-  }
+  };
+
+  if (editing)
+    return (
+      <ProfileCard.Edit title={title} handleClick={toggleEditMode} handleSave={handleSave}>
+        {Object.entries(data).map(([k, v]) => (
+          <ProfileCardItem.Edit
+            key={k}
+            name={k}
+            data={v}
+            fields={newData}
+            setData={setNewData}
+          />
+        ))}
+      </ProfileCard.Edit>
+    );
 
   return (
-    <ProfileCard title={title} canEdit={canEdit} handleClick={handleClick}>
-      {isEditing
-        ? Object.entries(data).map(([k, v]) => (
-            <ProfileCardItem.Edit key={k} name={k} data={v} fields={newData} setData={setNewData} />
-          ))
-        : Object.values(data).map((e, i) => (
-            <ProfileCardItem
-              key={i}
-              title={e.text}
-              value={e.value}
-              formatter={e.formatter}
-            />
-          ))}
-      {isEditing && (
-        <div className="d-flex justify-content-center">
-          <Button id="save-profile" variant="primary" type="submit" onClick={handleSave}>
-            Salvar
-          </Button>
-        </div>
-      )}
+    <ProfileCard title={title} canEdit={canEdit} handleClick={toggleEditMode}>
+      {Object.values(data).map((e, i) => (
+        <ProfileCardItem
+          key={i}
+          title={e.text}
+          value={e.value}
+          formatter={e.formatter}
+        />
+      ))}
     </ProfileCard>
   );
 };
--- frontend/src/components/Profile/ProfileCard/ProfileCard.jsx
+++ frontend/src/components/Profile/ProfileCard/ProfileCard.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Card, Container } from 'react-bootstrap';
+import { Button, Card, Container } from 'react-bootstrap';
 import './ProfileCard.css';
 
 const ProfileCard = ({ title, canEdit, handleClick, children }) => {
-
-
   return (
     <Container fluid="md" className="py-2">
       <Card className="card-profile">
@@ -25,5 +23,36 @@ const ProfileCard = ({ title, canEdit, handleClick, children }) => {
     </Container>
   );
 };
+
+const Edit = ({ title, handleClick, handleSave, children }) => {
+  return (
+    <Container fluid="md" className="py-2">
+      <Card className="card-profile">
+        <Card.Body className="p-2 p-md-3">
+          {title && (
+            <Card.Title className="d-flex justify-content-between align-items-center">
+              {title}
+              <button className="btn-profile-edit" onClick={handleClick}>
+                <i className="far fa-edit"></i>
+              </button>
+            </Card.Title>
+          )}
+          {children}
+          <div className="d-flex justify-content-center">
+            <Button
+              id="save-profile"
+              variant="primary"
+              type="submit"
+              onClick={handleSave}
+            >
+              Salvar
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+ProfileCard.Edit = Edit;
 
 export default ProfileCard;
