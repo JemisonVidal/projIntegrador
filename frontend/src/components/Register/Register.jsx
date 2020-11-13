8735f421d2a570f06c0a61ebb31287c7806af40d
jemisonsantos <jemison.santos@catho.com.br>
2020-11-13 20:05:38 -0300

    removed termo

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Form, Button, Spinner } from 'react-bootstrap';
-import { useHistory, Link } from 'react-router-dom';
-import Input from '../../components/Input/Input';
-import Checkbox from '../../components/Checkbox/Checkbox';
-import Error from '../../components/Helper/Error';
-import useForm from '../../Hooks/useForm';
-import useFetch from '../../Hooks/useFetch';
-import { USER_REGISTER } from '../../APIs/APIs';
-import './Register.css';
+import React from "react";
+import { Form, Button, Spinner } from "react-bootstrap";
+import { useHistory, Link } from "react-router-dom";
+import Input from "../../components/Input/Input";
+import Error from "../../components/Helper/Error";
+import useForm from "../../Hooks/useForm";
+import useFetch from "../../Hooks/useFetch";
+import { USER_REGISTER } from "../../APIs/userAPI";
+import "./Register.css";
 
 const UserRegister = () => {
   const nome = useForm();
-  const email = useForm('email');
+  const email = useForm("email");
   const senha = useForm();
   const confirmarSenha = useForm();
   const tipo = useForm();
@@ -20,13 +19,18 @@ const UserRegister = () => {
   const history = useHistory();
 
   const optionsComboBox = [
-    {value: '', text: 'Escolha uma opção'},
-    {value: 'APPLICANT', text: 'Quero trabalhar'},
-    {value: 'COMPANY', text: 'Quero recrutar'},
+    { value: "", text: "Escolha uma opção" },
+    { value: "APPLICANT", text: "Quero trabalhar" },
+    { value: "COMPANY", text: "Quero recrutar" },
   ];
 
   async function registrar(nome, email, senha, tipo) {
-    const { url, options } = USER_REGISTER({ name: nome.value, email: email.value, password: senha.value, type: tipo.value });
+    const { url, options } = USER_REGISTER({
+      name: nome.value,
+      email: email.value,
+      password: senha.value,
+      type: tipo.value,
+    });
     return await request(url, options);
   }
 
@@ -38,12 +42,11 @@ const UserRegister = () => {
       email.validate() &&
       senha.validate() &&
       confirmarSenha.validate() &&
-      tipo.validate() && 
-      termo.value() &&
+      tipo.validate() &&
       senha.value === confirmarSenha.value
     ) {
       const { response } = await registrar(nome, email, senha, tipo);
-      if (response.ok) return history.push('/login');
+      if (response?.ok) return history.push("/login");
     }
   }
 
@@ -87,12 +90,6 @@ const UserRegister = () => {
             options={optionsComboBox}
             {...tipo}
           />
-          <Checkbox
-            className="CheckBoxMin"
-            type="checkbox"
-            required
-            label="Eu concordo com os termos de serviço e políticas de privacidade."
-          />
           {loading ? (
             <Button id="Cadastrar" variant="primary" disabled>
               <Spinner
@@ -105,16 +102,16 @@ const UserRegister = () => {
               <span className="sr-only">Carregando...</span>
             </Button>
           ) : (
-              <Button id="Cadastrar" variant="primary" type="submit">
-                Cadastrar
-              </Button>
-            )}
+            <Button id="Cadastrar" variant="primary" type="submit">
+              Cadastrar
+            </Button>
+          )}
           <Error error={error} />
           <p id="cadastro">
-            Já tem cadastro ?{' '}
+            Já tem cadastro ?{" "}
             <Link id="login" to="/login">
               Faça o Login
-          </Link>
+            </Link>
           </p>
         </Form>
       </div>
