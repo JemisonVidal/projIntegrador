e1bfdfea4d36b68549526ac8eb02a0cf0bde1546
priscillatiemi <prips.tiemi@gmail.com>
2020-11-23 22:33:25 -0300

    PI-73
    atualização do texto de Terms 1. Aceitação dos Termos e Condições de uso

--- frontend/src/components/Terms/Terms.jsx
+++ frontend/src/components/Terms/Terms.jsx
@@ -30,35 +30,13 @@ const Terms = () => {
                 </h2>
                 <br></br>
                 <span>
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem aperiam, eaque
-                  ipsa quae ab illo inventore veritatis et quasi architecto
-                  beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
-                  quia voluptas sit aspernatur aut odit aut fugit, sed quia
-                  consequuntur magni dolores eos qui ratione voluptatem sequi
-                  nesciunt. Neque porro quisquam est, qui dolorem ipsum quia
-                  dolor sit amet, consectetur, adipisci velit, sed quia non
-                  numquam eius modi tempora incidunt ut labore et dolore magnam
-                  aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-                  nostrum exercitationem ullam corporis suscipit laboriosam,
-                  nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum
-                  iure reprehenderit qui in ea voluptate velit esse quam nihil
-                  molestiae consequatur, vel illum qui dolorem eum fugiat quo
-                  voluptas nulla pariatur? Sed ut perspiciatis unde omnis iste
-                  natus error sit voluptatem accusantium doloremque laudantium,
-                  totam rem aperiam, eaque ipsa quae ab illo inventore veritatis
-                  et quasi architecto beatae vitae dicta sunt explicabo. Nemo
-                  enim ipsam voluptatem quia voluptas sit aspernatur aut odit
-                  aut fugit, sed quia consequuntur magni dolores eos qui ratione
-                  voluptatem sequi nesciunt. Neque porro quisquam est, qui
-                  dolorem ipsum quia dolor sit amet, consectetur, adipisci
-                  velit, sed quia non numquam eius modi tempora incidunt ut
-                  labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
-                  minima veniam, quis nostrum exercitationem ullam corporis
-                  suscipit laboriosam, nisi ut aliquid ex ea commodi
-                  consequatur? Quis autem vel eum iure reprehenderit qui in ea
-                  voluptate velit esse quam nihil molestiae consequatur, vel
-                  illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+                  Ao acessar ao site RecruIT, concorda em cumprir estes termos
+                  de serviço, todas as leis e regulamentos aplicáveis ​​e
+                  concorda que é responsável pelo cumprimento de todas as leis
+                  locais aplicáveis. Se você não concordar com algum desses
+                  termos, está proibido de usar ou acessar este site. Os
+                  materiais contidos neste site são protegidos pelas leis de
+                  direitos autorais e marcas comerciais aplicáveis.
                 </span>
               </Card.Text>
               <Card.Text className="terms-text">
