d08d024d975df5e930d0988581783362d11d06e1
jemisonsantos <jemison.santoos@gmail.com>
2020-11-27 20:36:57 -0300

    removed tab my opportunity for user company

--- frontend/src/components/ListOpportunity/TabListOpportunity.js
+++ frontend/src/components/ListOpportunity/TabListOpportunity.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Container, Tab, Tabs } from "react-bootstrap";
 import Main from "../Template/main/Main";
 import { ALL_OPPORTUNITYS, MY_OPPORTUNITYS } from "./listEnum";
 import ListOpportunity from "./ListOpportunity";
+import StoreContext from "../../components/Store/Context";
 
 const TabListOpportunity = () => {
   const [key, setKey] = useState("allOpportunity");
+  const { user } = useContext(StoreContext);
   return (
     <Main>
       <Container fluid="md" className="py-2">
@@ -17,9 +19,11 @@ const TabListOpportunity = () => {
           <Tab eventKey="allOpportunity" title="Todas Oportunidades">
             {<ListOpportunity type={ALL_OPPORTUNITYS} />}
           </Tab>
-          <Tab eventKey="myOpportunity" title="Minhas Oportunidades">
-            {<ListOpportunity type={MY_OPPORTUNITYS} />}
-          </Tab>
+          {user?.type === "applicant" && (
+            <Tab eventKey="myOpportunity" title="Minhas Oportunidades">
+              {<ListOpportunity type={MY_OPPORTUNITYS} />}
+            </Tab>
+          )}
         </Tabs>
       </Container>
     </Main>
